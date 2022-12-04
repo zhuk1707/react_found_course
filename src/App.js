9719cb3ff17438f0c440ba2a5f3a4be291b8b2b5
Web Monkey <104577087+zhuk1707@users.noreply.github.com>
2022-12-04 16:30:26 +0300

    code decompose / create custom hooks

--- src/App.js
+++ src/App.js
@@ -1,59 +1,19 @@
-import React, {useMemo, useState} from "react";
+import React, {useState} from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: 'First title',
-      postContent: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-    },
-    {
-      id: 2,
-      title: 'Second title',
-      postContent: 'A aliquam debitis distinctio enim magni nihil nobis perspiciatis praesentium, quos, reprehenderit unde, veritatis voluptatum.'
-    },
-    {
-      id: 3,
-      title: 'Third title',
-      postContent: 'Adipisci aspernatur cum, deserunt enim eos hic illum ipsam magnam modi nesciunt quas quidem quisquam repellat sunt vitae?'
-    },
-    {
-      id: 4,
-      title: 'Fourth title',
-      postContent: 'Pulvinar sapien et ligula ullamcorper malesuada.'
-    },
-    {
-      id: 5,
-      title: 'Fifth title',
-      postContent: 'Diam maecenas sed enim ut sem viverra. Ullamcorper sit amet risus nullam.'
-    }
-  ])
-
+  const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''})
-
   const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   //------------------
-  //useMemo - Memoization, caching
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => {
-        a[filter.sort].localeCompare(b[filter.sort])
-      })
-    }
-    return posts
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
-
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal(false)
@@ -81,12 +41,14 @@ function App() {
       <hr/>*/}
 
       <MyButton
-        onClick={() => {setModal(true)}}
+        onClick={() => {
+          setModal(true)
+        }}
         style={{
           marginTop: '20px',
           borderColor: '#7000b7',
           width: '100%',
-      }}>
+        }}>
         Create post &#43;
       </MyButton>
 
@@ -99,10 +61,14 @@ function App() {
         setFilter={setFilter}
       />
 
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Posts'}/>
+      <PostList remove={removePost} posts={sortedAndSearchedPosts}
+                title={'Posts'}/>
 
       <div className={'copyright'}>
-        App created by <a href="https://github.com/zhuk1707">Zhuk1707</a> with <a href="https://www.youtube.com/watch?v=GNrdg3PzpJQ&t=4157s&ab_channel=UlbiTV">Ulbi TV</a> help. 2022
+        App created by <a
+        href="https://github.com/zhuk1707">Zhuk1707</a> with <a
+        href="https://www.youtube.com/watch?v=GNrdg3PzpJQ&t=4157s&ab_channel=UlbiTV">Ulbi
+        TV</a> help. 2022
       </div>
     </div>
   );
---
+++ src/hooks/usePosts.js
@@ -0,0 +1,21 @@
+import {useMemo} from "react";
+
+export const useSortedPosts = (posts, sort) => {
+  //useMemo - Memoization, caching
+  return useMemo(() => {
+    if (sort) {
+      return [...posts].sort((a, b) => {
+        a[sort].localeCompare(b[sort])
+      })
+    }
+    return posts
+  }, [sort, posts])
+}
+
+export const usePosts = (posts, sort, query) => {
+  const sortedPosts = useSortedPosts(posts, sort)
+
+  return useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(query))
+  }, [query, sortedPosts])
+}
